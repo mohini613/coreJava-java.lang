be79cadc5569507f10c543802ddad2dec9abac92
mohini613 <161958801+mohini613@users.noreply.github.com>
2024-09-02 00:32:25 +0530

    Add files via upload

---
+++ ReplaceIndexOf.java
@@ -0,0 +1,13 @@
+package com.java.lang;
+
+public class ReplaceIndexOf {
+
+	public static void main(String[] args) {
+		String s=new String ("hello java");
+		System.out.println(s.replace('h', 'H'));
+		System.out.println(s.replace('j', 'J'));
+		System.out.println(s.indexOf('h'));
+		System.out.println(s.indexOf('l'));
+		
+	}
+}
---
+++ SplitMethod.java
@@ -0,0 +1,37 @@
+package com.java.lang;
+
+public class SplitMethod {
+		
+	public void Split(String Split)
+
+	{
+			int []x= {10,20,30,40,50,};
+			char ch[]= {'a','b','c','d','e','f'};
+			String [] s= {"sachin", "pravin", "rohit", "ganesh"};
+	}
+		
+			String s= new String("10,20,30,40,50,a,b,c,d,e,f,sachin, pravin, rohit,ganesh");
+			
+			//public static void main(String[] args) {
+				//A a = new A();
+				//a.Split(",");
+				 String[] split;{
+				 split = s.split(",");
+				for (String sp:split){
+					System.out.println(sp);
+					
+					//System.out.println(s.indexOf());
+					//System.out.println(s.toUpperCase());
+					
+				}
+				}
+				public static void main(String[] args) {
+					SplitMethod  s = new SplitMethod ();
+			        s.Split(",");	
+	                //a.toUppercase(S);
+				 }
+	}
+		
+		
+
+
